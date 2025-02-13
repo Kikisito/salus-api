51626829368a89228479c81047ed99e9d1e8a760
Kikisito <bekikisito@gmail.com>
2025-02-14 00:49:04 +0100

    Bugfix: ControllerExceptionHandler no era REST
    Por algún motivo, duplicaba el path dos veces al recibirla

--- src/main/java/com/kikisito/salus/api/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/kikisito/salus/api/handlers/ControllerExceptionHandler.java
@@ -12,9 +12,9 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -22,7 +22,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
